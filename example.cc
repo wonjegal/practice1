c7f031e25b13676694960119ceb95233551b5e04
wonjegal <https://github.com/wonjegal/practice1.git>
2023-09-06 06:01:54 +0900

    add example.cc pc.cc pc.h

---
+++ example.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "pc.h"
+using namespace std;
+
+int main(void){
+    cout << "Hello world\n";
+    print_count();
+}
---
+++ pc.cc
@@ -0,0 +1,10 @@
+#include "pc.h"
+#include <iostream>
+using namespace std;
+
+void print_count(){
+    for(int i = 0; i < 10; i++){
+        cout << i << endl;
+    }
+    cout << endl;
+}
